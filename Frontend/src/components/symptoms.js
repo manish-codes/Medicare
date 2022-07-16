d47ee7029f7e0b47d0a339eb5dca91fb89ee4224
Manish <Manish1995g@gmail.com>
2022-07-16 19:04:11 +0530

    symptoms updated

--- Frontend/src/components/symptoms.js
+++ Frontend/src/components/symptoms.js
@@ -2,17 +2,17 @@ const disease=[
     {
         symptom:"Headache",
         disease:"Fever",
-        precaution:["layback","sit","relax"]
+        precaution:["Rest","Drink Fluids","Maintain Healthy Diet"]
     },
     {
-        symptom:"Diarrohea",
-        disease:"Typhoid",
-        precaution:["layback","sit","relax"]
+        symptom:"Red Eye",
+        disease:"Conjunctivitis",
+        precaution:["Wash eye Offently","Use soft Cloth for eye","Good Personal Hygiene"]
     },
     {
-        symptom:"Low sugar level",
-        disease:"Diabetes",
-        precaution:["layback","sit","relax"]
+        symptom:"Painful Joints",
+        disease:"Goitre",
+        precaution:["Increase intake of Iodine","Drink fluids"]
     },
     {
         symptom:"Difficulty in breathing",
@@ -23,6 +23,76 @@ const disease=[
         symptom:"High heart rate",
         disease:"Heart Disease",
         precaution:["layback","sit","relax"]
+    },
+    {
+        symptom:"Clay Colored Stool",
+        disease:"Hepatitis",
+        precaution:["Keep surrounding clean","Maintain Healthy Diet"]
+    },
+    {
+        symptom:"Malaise",
+        disease:"Influenza",
+        precaution:["Vaccination","Drink Fluids","Rest"]
+    },
+    {
+        symptom:"swollen eyelids",
+        disease:"Measles",
+        precaution:["Vaccination","Improve Hygiene"]
+    },
+    {
+        symptom:"arthralgia",
+        disease:"Meningitis",
+        precaution:["Vaccination","Improve Hygiene"]
+    },
+    {
+        symptom:"Swolln Lymph Nodes",
+        disease:"Monkey Pox",
+        precaution:["Vaccination","Improve Hygiene"]
+    },
+    {
+        symptom:"Difficulty in Chewing",
+        disease:"Mumps",
+        precaution:["Vaccination","Drink Fluids"]
+    },
+    {
+        symptom:"Chest pain With High Fever",
+        disease:"Pneumonia",
+        precaution:["Vaccination","Improve Hygiene"]
+    },
+    {
+        symptom:"acute respiratory distress",
+        disease:"Avian",
+        precaution:["Vaccination","Improve Dining Hygiene"]
+    },
+    {
+        symptom:"Swelling of airways",
+        disease:"pertussis",
+        precaution:["Vaccination","Improve Hygiene"]
+    },
+    {
+        symptom:"Nasal discharge",
+        disease:"Diphtheria",
+        precaution:["Vaccination","Improve Hygiene","Drink Fluids"]
+    },
+    {
+        symptom:"Fever, headache, nausea",
+        disease:"Malaria",
+        precaution:["Vaccination","Improve Hygiene","Drink Fluids"]
+    },
+    {
+        symptom:"Deformed Skull",
+        disease:"Rickets",
+        precaution:["Take regular sunbaths","Increase intake of vitmain D"]
+    },
+    {
+        symptom:"Dryness",
+        disease:"Xerophthalmia",
+        precaution:["Increase Hygiene","Increase intake of vitmain A"]
+    },
+    {
+        symptom:"Sore Throat",
+        disease:"Diphtheria",
+        precaution:["Increase Hygiene","Increase intake of vitmains"]
     }
 ]
 module.exports=disease;
